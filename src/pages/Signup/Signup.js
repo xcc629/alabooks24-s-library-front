bb21b0d76b3ec7773ef537a546b89abb1c25095a
geunhong Lim <seelime11@gmail.com>
2022-04-14 01:17:32 +0900

    [add] 로그인 에러체크, 에러 표시 추가

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,4 +1,5 @@
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Helmet } from "react-helmet";
 
 import SignNav from "../../components/SignNav/SignNav";
@@ -6,27 +7,182 @@ import GreenButton from "../../components/GreenButton/GreenButton";
 import SignupForm from "../../components/SignupForm/SignupForm";
 import "./Signup.scss";
 import "../../components/SignupForm/SignupForm.module.css";
+import { ImWarning } from "react-icons/im";
 
 function Signup() {
-  const [idValue, setIdValue] = useState("");
-  const [pwValue, setpwValue] = useState("");
-  const [rePwValue, setRePwValue] = useState("");
-  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+  const [signupValueObj, setsignupValueObj] = useState({
+    loginId: "",
+    password: "",
+    passwordConfirm: "",
+    emailAddress: "",
+  });
+  const [valueError, setvalueError] = useState({
+    loginIdErrMessage: "",
+    passwordErrMessage: "",
+    passwordConfirmErrMessage: "",
+    emailAddressErrMessage: "",
+  });
 
-  const users = {
-    loginId: idValue,
-    password: pwValue,
-    passwordConfirm: rePwValue,
-    emailAddress: email,
+  const goToLogin = () => {
+    navigate("/login");
+  };
+
+  const totalValidation = () => {
+    idValidation();
+    passwordValidation();
+    passwordConfirmValidation();
+    emailAddressValidation();
+  };
+
+  const idValidation = () => {
+    const { loginId } = signupValueObj;
+    const regId = /^[a-zA-Z0-9]{5,20}$/;
+    if (loginId.length === 0) {
+      setvalueError((prev) => {
+        return {
+          ...prev,
+          loginIdErrMessage: "아이디를 입력해주세요",
+        };
+      });
+      return;
+    } else if (!regId.test(loginId)) {
+      setvalueError((prev) => {
+        return {
+          ...prev,
+          loginIdErrMessage: "아이디가 형식에 맞지 않습니다.",
+        };
+      });
+      return;
+    }
+    setvalueError((prev) => {
+      return {
+        ...prev,
+        loginIdErrMessage: "",
+      };
+    });
+  };
+
+  const passwordValidation = () => {
+    const { password } = signupValueObj;
+    const regPassword =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
+
+    if (password.length === 0) {
+      setvalueError((prev) => {
+        return { ...prev, passwordErrMessage: "비밀번호를 입력해주세요" };
+      });
+      return;
+    } else if (!regPassword.test(password)) {
+      setvalueError((prev) => {
+        return {
+          ...prev,
+          passwordErrMessage:
+            "8자 이상, 영문/숫자/특수문자 포함하여 입력해주세요.",
+        };
+      });
+      return;
+    }
+
+    setvalueError((prev) => {
+      return {
+        ...prev,
+        passwordErrMessage: "",
+      };
+    });
+  };
+
+  const passwordConfirmValidation = () => {
+    const { password, passwordConfirm } = signupValueObj;
+    if (passwordConfirm.length === 0) {
+      setvalueError((prev) => {
+        return {
+          ...prev,
+          passwordConfirmErrMessage: "비밀번호를 재입력해주세요",
+        };
+      });
+      return;
+    } else if (password !== passwordConfirm) {
+      setvalueError((prev) => {
+        return {
+          ...prev,
+          passwordConfirmErrMessage: "비밀번호가 일치하지 않습니다.",
+        };
+      });
+      return;
+    }
+    setvalueError((prev) => {
+      return {
+        ...prev,
+        passwordConfirmErrMessage: "",
+      };
+    });
+  };
+
+  const emailAddressValidation = () => {
+    const { emailAddress } = signupValueObj;
+    const regemailAddress = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+(.)[a-zA-Z0-9]+$/;
+
+    if (emailAddress.length === 0) {
+      setvalueError((prev) => {
+        return { ...prev, emailAddressErrMessage: "이메일을 입력해주세요" };
+      });
+      return;
+    } else if (!regemailAddress.test(emailAddress)) {
+      setvalueError((prev) => {
+        return {
+          ...prev,
+          emailAddressErrMessage: "이메일 형식이 맞지 않습니다",
+        };
+      });
+      return;
+    }
+    setvalueError((prev) => {
+      return {
+        ...prev,
+        emailAddressErrMessage: "",
+      };
+    });
   };
 
   const onClick = () => {
-    postHandler();
+    totalValidation();
+    const {
+      loginIdErrMessage,
+      passwordErrMessage,
+      passwordConfirmErrMessage,
+      emailAddressErrMessage,
+    } = valueError;
+
+    if (
+      !loginIdErrMessage.length &&
+      !passwordErrMessage.length &&
+      !passwordConfirmErrMessage.length &&
+      !emailAddressErrMessage.length
+    ) {
+      postHandler();
+      goToLogin();
+    }
   };
 
-  const onKeyDown = (e) => {
+  const onKeyDownEnter = (e) => {
+    const {
+      loginIdErrMessage,
+      passwordErrMessage,
+      passwordConfirmErrMessage,
+      emailAddressErrMessage,
+    } = valueError;
     if (e.keyCode === 13) {
-      postHandler();
+      totalValidation();
+      if (
+        !loginIdErrMessage.length &&
+        !passwordErrMessage.length &&
+        !passwordConfirmErrMessage.length &&
+        !emailAddressErrMessage.length
+      ) {
+        postHandler();
+        goToLogin();
+      }
     }
   };
 
@@ -36,7 +192,7 @@ function Signup() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(users),
+      body: JSON.stringify(signupValueObj),
     })
       .then((res) => res.json())
       .then((result) => console.log(result));
@@ -55,71 +211,98 @@ function Signup() {
               data.id === 1 && (
                 <SignupForm
                   key={data.id}
+                  name={"loginId"}
                   div={data.div}
                   after={data.after}
                   type={data.type}
-                  setValue={setIdValue}
-                  onKeyDown={onKeyDown}
+                  setValue={setsignupValueObj}
+                  onKeyDownEnter={onKeyDownEnter}
+                  onValidate={idValidation}
                 />
               )
             );
           })}
         </div>
-        <span className={idValue > 4 ? "warningDown" : "warningUp"}>
-          ! 5~10자의 영문 소문자와 숫자로만 입력해주세요.
-        </span>
-        <div className="signupPasswordsWap">
-          {datas.map((data) => {
-            return (
-              data.id === 2 && (
-                <SignupForm
-                  key={data.id}
-                  div={data.div}
-                  after={data.after}
-                  type={data.type}
-                  setValue={setpwValue}
-                  onKeyDown={onKeyDown}
-                />
-              )
-            );
-          })}
-        </div>
-        <div className="signupRePasswordsWap">
-          {datas.map((data) => {
-            return (
-              data.id === 3 && (
-                <SignupForm
-                  key={data.id}
-                  div={data.div}
-                  after={data.after}
-                  type={data.type}
-                  setValue={setRePwValue}
-                  onKeyDown={onKeyDown}
-                />
-              )
-            );
-          })}
-        </div>
-        <span className={pwValue === "" ? "warningUp" : "warnginDown"}>
-          ! 비밀번호를 입력해주세요.
-        </span>
+        {valueError.loginIdErrMessage.length !== 0 && (
+          <span className="warningUp">
+            <ImWarning />
+            {` ${valueError.loginIdErrMessage}`}
+          </span>
+        )}
+        <form>
+          <div className="signupPasswordsWap">
+            {datas.map((data) => {
+              return (
+                data.id === 2 && (
+                  <SignupForm
+                    key={data.id}
+                    name={"password"}
+                    div={data.div}
+                    after={data.after}
+                    type={data.type}
+                    setValue={setsignupValueObj}
+                    onKeyDownEnter={onKeyDownEnter}
+                    onValidate={passwordValidation}
+                  />
+                )
+              );
+            })}
+          </div>
+          <div className="signupRePasswordsWap">
+            {datas.map((data) => {
+              return (
+                data.id === 3 && (
+                  <SignupForm
+                    key={data.id}
+                    name={"passwordConfirm"}
+                    div={data.div}
+                    after={data.after}
+                    type={data.type}
+                    setValue={setsignupValueObj}
+                    onKeyDownEnter={onKeyDownEnter}
+                    onValidate={passwordConfirmValidation}
+                  />
+                )
+              );
+            })}
+          </div>
+        </form>
+        {(valueError.passwordErrMessage ||
+          valueError.passwordConfirmErrMessage) && (
+          <span className="warningUp">
+            <ImWarning />
+            {` ${
+              valueError.passwordErrMessage ||
+              valueError.passwordConfirmErrMessage
+            }`}
+          </span>
+        )}
+
         <div className="signupEmail">
           {datas.map((data) => {
             return (
               data.id === 4 && (
                 <SignupForm
                   key={data.id}
+                  name={"emailAddress"}
                   div={data.div}
                   after={data.after}
                   type={data.type}
-                  setValue={setEmail}
-                  onKeyDown={onKeyDown}
+                  setValue={setsignupValueObj}
+                  onKeyDownEnter={onKeyDownEnter}
+                  onValidate={emailAddressValidation}
                 />
               )
             );
           })}
         </div>
-        <span className="warning">! 이메일을 입력해주세요.</span>
+        {valueError.emailAddressErrMessage && (
+          <span className="warningUp">
+            <ImWarning />
+            {` ${valueError.emailAddressErrMessage}`}
+          </span>
+        )}
+
         <GreenButton span={"회원 가입 완료"} onClick={onClick} />
       </section>
     </div>
@@ -130,28 +313,24 @@ export default Signup;
 const datas = [
   {
     id: 1,
-
     div: "아이디",
-    after: "5~10자 영문, 숫자",
+    after: "5~20자 영문, 숫자",
     type: "text",
   },
   {
     id: 2,
-
     div: "비밀번호",
-    after: "8자 이상",
+    after: "8자 이상, 영문/숫자/특수문자 포함하여 입력",
     type: "password",
   },
   {
     id: 3,
-
     div: "비밀번호 확인",
     after: "비밀번호 재입력",
     type: "password",
   },
   {
     id: 4,
-
     div: "이메일 주소",
     after: "이메일 주소 입력",
     type: "email",
