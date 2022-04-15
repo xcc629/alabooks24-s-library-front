9e00958ad3ec07e83ec89bbdc8962bf2f7a69ab8
geunhong Lim <seelime11@gmail.com>
2022-04-15 23:38:28 +0900

    [fix] 지금 많이 읽고 있는 책 슬라이드 고침

--- src/components/Slide/NextArrow.js
+++ src/components/Slide/NextArrow.js
@@ -1,9 +1,16 @@
+import styled from "styled-components";
+
 export default function NextArrow(props) {
   const { className, style, onClick } = props;
   return (
     <div
       className={className}
-      style={{ ...style, display: "block", background: "red" }}
+      style={{
+        ...style,
+        display: "block",
+        background: "red",
+        size: 300,
+      }}
       onClick={onClick}
     />
   );
